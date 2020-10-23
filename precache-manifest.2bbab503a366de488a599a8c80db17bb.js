697ac2e7412e16d5b1d122ce67583d09daeea115
Victorowsky <62756503+Victorowsky@users.noreply.github.com>
2020-10-23 16:51:29 +0200

    Add files via upload

---
+++ precache-manifest.2bbab503a366de488a599a8c80db17bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41688651f1161da375ae5aed52320cae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0937a7d04870dde783e4",
+    "url": "./static/css/main.a0327842.chunk.css"
+  },
+  {
+    "revision": "b02f1ffc6f2c4a252f65",
+    "url": "./static/js/2.b4b77aff.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "./static/js/2.b4b77aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0937a7d04870dde783e4",
+    "url": "./static/js/main.d6151ee7.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
