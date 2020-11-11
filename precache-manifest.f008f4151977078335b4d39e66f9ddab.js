4a894047afa5f81342215cba5341bfa9c3d5c41a
Victorowsky <Victorowskyy@gmail.com>
2020-11-11 21:51:57 +0100

    deploy

---
+++ precache-manifest.f008f4151977078335b4d39e66f9ddab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42eff68c7ed9698cb763215e48a6def",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2788685c3f19afa9591",
+    "url": "./static/css/main.57ebc6a6.chunk.css"
+  },
+  {
+    "revision": "48da2db833362c11933b",
+    "url": "./static/js/2.b2e414db.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "./static/js/2.b2e414db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2788685c3f19afa9591",
+    "url": "./static/js/main.ca450e81.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
